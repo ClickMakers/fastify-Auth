9a7c24ecb0197a9f3f4a94055733cb0937161b95
ClickMakers <89856868+ClickMakers@users.noreply.github.com>
2022-05-10 18:29:17 +0530

    Changes-In-env

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+
+require('dotenv').config()
+
 // Require the framework and instantiate it
 const fastify = require('fastify')({ logger: true })
+
 
 // Declare a route
 fastify.get('/', async (request, reply) => {
@@ -12,7 +16,7 @@ fastify.register(require('./routes/Profiles'), { prefix: '/v1/users' })
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(3000)
+    await fastify.listen(process.env.PORT)
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
